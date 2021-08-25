cb2766ac613dfc5ce252390a5df4db75825ef8d5
Sam Vivonia <snvivonia@gmail.com>
2021-08-24 19:01:58 -0700

    listen for 'submit' events on the journal entry form, feature 1

--- js/main.js
+++ js/main.js
@@ -2,8 +2,27 @@
 /* exported data */
 
 const $imgPreview = document.querySelector('.img-preview');
+const $title = document.querySelector('#title');
 const $photoURL = document.querySelector('#photo-url');
+const $notes = document.querySelector('#notes');
 
 $photoURL.addEventListener('input', function () {
   $imgPreview.setAttribute('src', $photoURL.value);
+});
+
+const $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var newEntry = {
+    title: $title.value,
+    photoURL: $photoURL.value,
+    notes: $notes.value,
+    nextEntryID: 0
+  };
+  // console.log(newEntry);
+  newEntry.nextEntryID = newEntry.nextEntryID++;
+  data.entries.unshift(newEntry);
+  $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
