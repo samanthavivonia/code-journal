f62fd16bfcba710ee0c986f0615ba554bcf6300a
Sam Vivonia <snvivonia@gmail.com>
2021-08-26 20:00:09 -0700

    Define function that creates DOM tree, loop through entries to render them, remove dummy examples.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,43 @@ $form.addEventListener('submit', function (event) {
   $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+// CREATE ENTRY DOM TREE
+
+function renderNewEntry(entry) {
+
+  var $entry = document.createElement('div');
+  $entry.setAttribute('class', 'row entry');
+
+  var $imgDivWrapper = document.createElement('div');
+  $imgDivWrapper.setAttribute('class', 'img column-half');
+  $entry.appendChild($imgDivWrapper);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('class', 'img-preview');
+  $img.setAttribute('src', entry.photoURL);
+  $img.setAttribute('alt', 'Entry Image Preview');
+  $imgDivWrapper.appendChild($img);
+
+  var $textDivWrapper = document.createElement('div');
+  $textDivWrapper.setAttribute('class', 'column-half');
+  $entry.appendChild($textDivWrapper);
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.textContent = entry.title;
+  $textDivWrapper.appendChild($entryTitle);
+
+  var $entryDesc = document.createElement('p');
+  $entryDesc.textContent = entry.notes;
+  $textDivWrapper.appendChild($entryDesc);
+
+  return $entry;
+}
+
+const $entriesList = document.querySelector('ul');
+
+window.addEventListener('DOMContentLoaded', function () {
+  for (var i = 0; i < data.entries.length; i++) {
+    $entriesList.appendChild(renderNewEntry(data.entries[i]));
+  }
+});
